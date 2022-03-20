f4765b306a117ea201bc2b06e562d29cbd204349
shua1090 <shynn.lawrence@gmail.com>
2022-03-19 20:40:14 -0700

    LLVM Linking complete

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include <map>
+
+using namespace llvm;
+
+
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, Value *> NamedValues;
+
+
+int main(){
+    std::cout << "Foo1s Hello" << std::endl;
+    return 0;
+}
--- src/antlr_generated/LuxLangBaseVisitor.cpp
+++
@@ -1,7 +0,0 @@
-
-// Generated from LuxLang.g4 by ANTLR 4.9.3
-
-
-#include "LuxLangBaseVisitor.h"
-
-
