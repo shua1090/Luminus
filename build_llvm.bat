384058101764ef473286eef20cb47eae856c58a5
Shynn Lawrence <shynn.lawrence@gmail.com>
2022-04-03 01:58:42 +0000

    Build Batchfile updated

--- build_llvm.bat
+++ build_llvm.bat
@@ -2,6 +2,6 @@ git clone --config core.autocrlf=false https://github.com/llvm/llvm-project.git 
 cd llvm-project
 mkdir build
 cd build
-cmake ../llvm -DLLVM_TARGETS_TO_BUILD="X86"
+cmake ../llvm -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_PROJECTS="" -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_BUILD_TOOLS=OFF -DLLVM_BUILD_UTILS=OFF -DLLVM_PARALLEL_LINK_JOBS=8 -G"Ninja"
 # In Admin:
-cmake --build . --config Debug --target install
+cmake --build . --config Debug --target install --parallel 8
